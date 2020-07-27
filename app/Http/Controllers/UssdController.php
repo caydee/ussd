f189c4377fe80da46dfe20cef42680465853778d
Jeffrey Korir <JKorir@STANDARDMEDIA.CO.KE>
2020-07-27 11:18:54 +0300

    Ussd Setup

---
+++ app/Http/Controllers/UssdController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UssdController extends Controller
+{
+    public function Menus(Request $request){
+        return "CON Menus Here";
+    }
+}
