358275fbd5b6eca546a0e5339a31ada42e646d58
Jeffrey Korir <JKorir@STANDARDMEDIA.CO.KE>
2020-08-10 11:05:52 +0300

    response

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Log;
 
 class UssdController extends Controller
