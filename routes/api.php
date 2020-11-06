a080fb0b7fc736262d2e25c632b39828e42ab6aa
Jeffrey Korir <JKorir@STANDARDMEDIA.CO.KE>
2020-11-06 10:16:29 +0300

    fallback

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::fallback(fn()=>[
+    'code'=>'404',
+    'reason'=>'Not Defined'
+]);
 
 Route::get('Request','UssdController@Request');
 Route::get('ussdmenus','UssdController@ussdmenus');
